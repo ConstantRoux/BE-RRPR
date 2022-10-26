b6b148ede244818392059b70d4ff7e029f0f88d7
simonrulof <simonber2001@gmail.com>
2022-10-26 09:45:26 +0200

    rework MDD

--- MDD.py
+++ MDD.py
@@ -4,17 +4,23 @@ import numpy as np
 class MDD:
 
     def __init__(self, L):
-        self.J = np.matrix([
-            [0, 0, 0, 0],
-            [L[0] + L[1] + L[2] + L[3], L[1] + L[2] + L[3], 0, 0],
+        self.L = L
+    def getJacobienne(self, q1, q2, q3, q4):
+        P22 = self.L[2] * np.cos(q2) * np.sin(q1) + self.L[2] * np.sin(q2) * np.cos(q1) + self.L[3] * np.sin(q1) * np.cos(q2) + self.L[3] * np.sin(q2) * np.cos(q1)
+        P21 = self.L[2] * np.cos(q2) * np.cos(q1) - self.L[2] * np.sin(q2) * np.sin(q1) + self.L[3] * np.cos(q2) * np.cos(q1) - self.L[3] * np.sin(q2) * np.sin(q1)
+        P11 = self.L[1] * np.cos(q1) + self.L[2] * np.cos(q2) * np.cos(q1) - self.L[2] * np.sin(q2) * np.sin(q1) + self.L[3] * np.cos(q2) * np.cos(q1) - self.L[3] * np.sin(q2) * np.sin(q1)
+        P12 = self.L[1] * np.sin(q1) + self.L[2] * np.cos(q2) * np.sin(q1) + self.L[2] * np.sin(q2) * np.cos(q1) + self.L[3] * np.cos(q2) * np.sin(q1) + self.L[3] * np.sin(q2) * np.cos(q1)
+        J = [
+            [-P12, -P22, 0, 0],
+            [P11, P21, 0, 0],
             [0, 0, 1, 0],
             [0, 0, 0, 0],
             [0, 0, 0, 0],
-            [1, 1, 0, 1]
-        ])
+            [1, 1, 0, 1],
+        ]
 
-    def getJacobienne(self):
-        return self.J
+
+        return J
 
     def getJacobienneInv(self):
         return np.linalg.inv(self.J)
